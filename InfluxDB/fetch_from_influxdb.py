f0727896d37a97c090d11772c0444f9e9c1a743a
Vijeet Nigam <vijeetnigam26@gmail.com>
2024-04-18 16:28:10 +0530

    InfluxDB Management Script

---
+++ InfluxDB/fetch_from_influxdb.py
@@ -0,0 +1,42 @@
+import json
+import pandas as pd
+from DataManager._influxdatabase import APIHandler
+
+token = "FN1XbuLPfmUJ5xFZpjdI9m0TRq1jeNEcmh305vVV9nexhHo7FTwPHNC9NWKUP4yxvu2qzGL8UaAjykZUiZkejA=="
+org = "self"
+url = "http://localhost:8086/"
+
+api = APIHandler(url, token, org)
+
+df = (api.fetch_time_series_data("2023-12-01", 
+                                "2024-01-01", 
+                                "1h", 
+                                ["API_Gravity", "Catalyst_Activity_%", "ConversionEfficiency_%", "FlowRate_m3_hr"], 
+                                ["CRUDE_AREA", "HYDRO_AREA"]))
+
+for i in df.columns:
+    df.rename(columns={i: f'{i[1]}_{i[0]}'}, inplace=True)
+
+print(df)
+
+df.index = pd.to_datetime(df.index)
+
+df.reset_index(inplace=True)
+
+df['Timestamp'] = pd.to_datetime(df['Timestamp'])
+
+df['Timestamp'] = df['Timestamp'].dt.strftime('%Y-%m-%d %H:%M:%S')
+
+json_data = df.to_json(orient='records')
+
+# json_data = df.to_json(orient="index")
+
+parsed_json = json.loads(json_data)
+formatted_json = json.dumps(parsed_json, indent=4)
+
+print(formatted_json)
+
+with open(r'InfluxDB//data_fetched//CRUDE&HYDRO_AREA_data.json', 'w') as f:
+            f.write(formatted_json)
+
+# api.fetch_time_series_data(startTime, stopTime, resamplingFreq, listofsensor_id, listofsensor_data)
